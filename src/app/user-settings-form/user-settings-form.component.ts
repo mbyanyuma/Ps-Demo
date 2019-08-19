40489e31ab65c54a7862cdf7c3f06554d12011db
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-08-19 09:13:20 -0400

    created userSettings data model for storing user form input, implemented two-way data binding

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../data/iuser-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  IUserSettings: IUserSettings = {
+    name: 'Moses',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'some notes'
+  };
 
   constructor() { }
 
