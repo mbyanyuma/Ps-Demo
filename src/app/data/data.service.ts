696d0d4e6ef432e50ea8204095dda531a64bf4d5
Moses Byanyuma <mbyanyuma108@gmail.com>
2019-09-04 09:33:38 -0400

    implemented http access using httpclient and form posting using our data service

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { IUserSettings } from './iuser-settings';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private theHttp: HttpClient) { }
 
-  postUserSettingsForm(userSettings: IUserSettings): Observable<IUserSettings> {
-    return of(userSettings);
+  postUserSettingsForm(userSettings: IUserSettings): Observable<any> {
+    // tslint:disable-next-line: no-trailing-whitespace
+    
+    return this.theHttp.post('https://putsreq.com/0gzkLkfEKNNOFX1frJdu', userSettings);
+    // tslint:disable-next-line: no-trailing-whitespace
+    
+    // return of(userSettings);
   }
 }
